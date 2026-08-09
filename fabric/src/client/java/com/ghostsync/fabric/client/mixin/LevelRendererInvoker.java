563daa52a1ada36570ba006f521b7100f9048c83
shaas562 <shaas@vagabondhouse.com>
2026-08-09 12:29:31 +0200

    Neutralize obsolete LevelRenderer bridge

--- fabric/src/client/java/com/ghostsync/fabric/client/mixin/LevelRendererInvoker.java
+++ fabric/src/client/java/com/ghostsync/fabric/client/mixin/LevelRendererInvoker.java
@@ -1,12 +1,10 @@
 package com.ghostsync.fabric.client.mixin;
 
-import net.minecraft.client.renderer.LevelRenderer;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Invoker;
-
-/** Narrow bridge to rebuild only terrain sections affected by ghost alpha changes. */
-@Mixin(LevelRenderer.class)
-public interface LevelRendererInvoker {
-    @Invoker("setSectionDirty")
-    void ghostsync$setSectionDirty(int sectionX, int sectionY, int sectionZ);
+/**
+ * Reserved package placeholder. Minecraft 26.2 no longer exposes the historical
+ * section-dirty method this bridge originally targeted.
+ */
+final class LevelRendererInvoker {
+    private LevelRendererInvoker() {
+    }
 }
